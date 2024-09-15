e6199be700b007e6e8986b4cde6ca10177f08d6a
Kavindi Fernando <it21388248@my.sliit.lk>
2024-09-15 15:26:53 +0530

    .env

--- RentMate/frontend/src/components/Customer_Profile/info_card/Info_card.jsx
+++ RentMate/frontend/src/components/Customer_Profile/info_card/Info_card.jsx
@@ -1,54 +1,57 @@
 import React, { useEffect, useState } from "react";
 import "./info_card.css";
 import axios from "axios";
-import imagePath from "../../../uploads/file-1684405353607-923478245.JPG";
+import imagePath from "../../../Images/preview.png";
 
 const Info_Card = () => {
   const [currentDate, setCurrentDate] = useState("");
 
   useEffect(() => {
     const date = new Date();
-    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const formattedDate = date.toLocaleDateString('en-US', options);
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+    const formattedDate = date.toLocaleDateString("en-US", options);
     setCurrentDate(formattedDate);
   }, []);
 
-//user data get
+  //user data get
   const [customerinfo, setCustomerinfo] = useState({});
   const [customerinfo2, setCustomerinfo2] = useState({});
-  
+
   const [avatarUrl, setAvatarUrl] = useState("");
 
   const userid = localStorage.getItem("id");
 
-  useEffect(() => { 
-  function getcustomerinforeal() {
-    axios(`http://localhost:7070/api/customer/customer_info/${userid}`)
-      .then((res) => {
-        setCustomerinfo2(res.data);
-        const temppic = res.data.avatar;
-        console.log(res.data);
-        const dynamicAvatarUrl = `../../../uploads/${temppic}`; // Build the dynamic URL based on the received data
+  useEffect(() => {
+    function getcustomerinforeal() {
+      axios(`http://localhost:7070/api/customer/customer_info/${userid}`)
+        .then((res) => {
+          setCustomerinfo2(res.data);
+          const temppic = res.data.avatar;
+          console.log(res.data);
+          const dynamicAvatarUrl = `../../../uploads/${temppic}`; // Build the dynamic URL based on the received data
           setAvatarUrl(dynamicAvatarUrl);
-        console.log(dynamicAvatarUrl);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  getcustomerinforeal()
-}, []);
-
+          console.log(dynamicAvatarUrl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    getcustomerinforeal();
+  }, []);
 
   // const pictureid = customerinfo2.avatar;
   // const imagePath = `../../../uploads/${pictureid}`;
-
 
   // console.log(imagePath);
 
   useEffect(() => {
     const userid = localStorage.getItem("id");
-  
+
     function getcustomerinfo() {
       axios(`http://localhost:7070/api/customer/customer_basic_info/${userid}`)
         .then((res) => {
@@ -62,15 +65,17 @@ const Info_Card = () => {
 
     getcustomerinfo();
   }, []);
-  
+
   return (
     <div>
       <div className="info_card">
         <div className="card1_back">
           <div className="profile_pic">
-          <img src={imagePath} alt="Profile Picture" />
+            <img src={imagePath} alt="Profile Picture" />
           </div>
-          <h3 className="profile_pic_h2">{customerinfo.fname} {customerinfo.lname}</h3>
+          <h3 className="profile_pic_h2">
+            {customerinfo.fname} {customerinfo.lname}
+          </h3>
           <h3>{customerinfo.email}</h3>
         </div>
         <div className="notification_card">
@@ -79,7 +84,9 @@ const Info_Card = () => {
               <h1 className="h1_notifi">Notifications</h1>
             </div>
             <div className="notification_date">
-              <h1 id="current_date" className="h1_notifi_date">{currentDate}</h1>
+              <h1 id="current_date" className="h1_notifi_date">
+                {currentDate}
+              </h1>
             </div>
           </div>
           <div className="notification_master">
