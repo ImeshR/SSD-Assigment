5df4b7f9962083ae5509a8f5c99edf6f3a818000
Eshan Imesh <111144332+ImeshR@users.noreply.github.com>
2024-09-22 13:12:50 +0530

    Updated unauthorized page

--- RentMate/frontend/src/Pages/Unauthorized/Unauthorized.jsx
+++ RentMate/frontend/src/Pages/Unauthorized/Unauthorized.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import styles from "../../components/com.style/missing.module.css";
+import { Link } from "react-router-dom";
 
 const Unauthorized = () => {
   return (
     <div>
-      <h1>Unauthorized</h1>
-      <p>You do not have permission to view this page.</p>
+      <div className={styles.page404}>
+        <div>
+          <div className={styles.errormsg}>
+            <h1>Unauthorized</h1>
+            <p>You do not have permission to view this page.</p>
+          </div>
+          <Link
+            to="/"
+            className={styles.link}
+          >
+            Go to Home
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
